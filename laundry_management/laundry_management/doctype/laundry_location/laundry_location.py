de77ef7ff0914661d114c29796a90ac0a656d2d6
derapper4567 <ashakisonga2022@gmail.com>
2025-01-05 20:47:48 +0300

    Add laundry_location files

---
+++ laundry_management/laundry_management/doctype/laundry_location/laundry_location.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Asha Melius Kisonga and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class LaundryLocation(Document):
+	pass
---
+++ laundry_management/laundry_management/doctype/laundry_location/test_laundry_location.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Asha Melius Kisonga and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLaundryLocation(FrappeTestCase):
+	pass
