d079864d15d5c047dd4b445df83c91406b22237c
derapper4567 <ashakisonga2022@gmail.com>
2025-01-15 22:45:45 +0300

    fix(print-format): update Print format

--- laundry_management/laundry_management/doctype/daily_task_schedule/daily_task_schedule.py
+++ laundry_management/laundry_management/doctype/daily_task_schedule/daily_task_schedule.py
@@ -1,9 +1,12 @@
 # Copyright (c) 2024, Asha Melius Kisonga and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class DailyTaskSchedule(Document):
-	pass
+    def before_insert(self):
+    
+        if not self.schedule_date:
+            self.schedule_date = frappe.utils.nowdate()  
--- laundry_management/laundry_management/page/laundry01/laundry01.js
+++ laundry_management/laundry_management/page/laundry01/laundry01.js
@@ -1,25 +1,54 @@
-frappe.pages['laundry01'].on_page_load = function(wrapper) {
-    var page = frappe.ui.make_app_page({
+frappe.pages['laundry01'].on_page_load = function (wrapper) {
+    const page = frappe.ui.make_app_page({
         parent: wrapper,
-        title: 'Daily Task Schedule',  
-        single_column: true
+        title: 'Daily Task Schedule',
+        single_column: true,
     });
 
+    const title_container = $('<div>').addClass('title-container');
 
-    page.set_primary_action('Schedule Date', function() {
-        frappe.msgprint('Schedule Date Action triggered');
+    
+    const marquee_title = $('<h2>')
+        .addClass('marquee-title')
+        .text('Daily Task Schedule');
+
+    
+    title_container.append(marquee_title);
+
+    
+    $(wrapper).find('.page-content').prepend(title_container);
+
+    // Append HTML
+    $(frappe.render_template('laundry01_page')).appendTo(page.body);
+    
+
+    frappe.call({
+        method: "laundry_management.laundry_management.page.laundry01.laundry01.get_daily_task_schedule",
+        callback: function(response) {
+            if (response.message) {
+                const data = response.message;
+                let tableBody = document.getElementById("task_schedule_table");
+                tableBody.innerHTML = ""; 
+                
+                
+                data.forEach(row => {
+                    let tr = document.createElement("tr");
+                    tr.innerHTML = `
+                        <td>${row.schedule_name}</td>
+                        <td>${row.task_name}</td>
+                        <td>${row.assigned_to}</td>
+                        <td>${row.location}</td>
+                        <td>${row.task_status}</td>
+                        <td>${row.schedule_date}</td>
+                    `;
+                    tableBody.appendChild(tr);
+                });
+            } else {
+                console.error("No data returned from server.");
+            }
+        },
+        error: function(error) {
+            console.error("Error in API call:", error);
+        }
     });
-
-
-
-
-	$(frappe.render_template("laundry01_page", {})).appendTo(page.body);
-};
-
-
-
-
-
-
-
-
+}
